6fba75be506e5e25e595361972e93730163a0bbc
wessantana <weslley.vieirasantana@gmail.com>
2025-07-24 21:44:27 -0300

    primeiro commit

---
+++ src/test/java/com/literatura/literal/LiteralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literatura.literal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
