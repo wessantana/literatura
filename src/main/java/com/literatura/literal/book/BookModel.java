4bffe04bdd8b6ddd6575789ae8a472a40e4d0f40
wessantana <weslley.vieirasantana@gmail.com>
2025-07-31 21:34:59 -0300

    feat(book) add class BookModel

---
+++ src/main/java/com/literatura/literal/book/BookModel.java
@@ -0,0 +1,33 @@
+package com.literatura.literal.book;
+
+import com.literatura.literal.chapter.ChapterModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false, length = 60)
+    private String title;
+
+    private String synopsis;
+
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "books", cascade = CascadeType.ALL)
+    private List<ChapterModel> chapters;
+
+    private List<String> tags;
+
+
+}
