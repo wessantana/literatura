4661e69583b3bb1ec401c3e3ebeefb9f1bc8fb66
wessantana <weslley.vieirasantana@gmail.com>
2025-08-05 19:40:40 -0300

    feat(book) create BookDTO class

---
+++ src/main/java/com/literatura/literal/book/BookDTO.java
@@ -0,0 +1,29 @@
+package com.literatura.literal.book;
+
+import com.literatura.literal.chapter.ChapterDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+
+    private String title;
+    private String synopsis;
+    private String imageUrl;
+    private List<ChapterDTO> chapters;
+    private List<String> tags;
+
+    public BookDTO(BookModel book){
+        this.title = book.getTitle();
+        this.synopsis = book.getSynopsis();
+        this.imageUrl = book.getImageUrl();
+        this.chapters = book.getChapters().stream().map(ChapterDTO::new).collect(Collectors.toList());
+        this.tags = book.getTags();
+    }
+}
