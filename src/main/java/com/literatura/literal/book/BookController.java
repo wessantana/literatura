2a365b02de5faee9c16d81474442379728e594a0
wessantana <weslley.vieirasantana@gmail.com>
2025-08-11 21:12:00 -0300

    feat(book) create Controller with getAll endpoint

---
+++ src/main/java/com/literatura/literal/book/BookController.java
@@ -0,0 +1,21 @@
+package com.literatura.literal.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    BookService service;
+
+    @GetMapping()
+    public List<BookDTO> getAllBooks() {
+        return service.getAllBooks();
+    }
+}
