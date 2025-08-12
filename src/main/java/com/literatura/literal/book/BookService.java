33d7c75fe3b8958868d81579f3929fe450486c51
wessantana <weslley.vieirasantana@gmail.com>
2025-08-11 21:20:12 -0300

    feat(book) add getAllBook funcion

--- src/main/java/com/literatura/literal/book/BookService.java
+++ src/main/java/com/literatura/literal/book/BookService.java
@@ -1,4 +1,22 @@
 package com.literatura.literal.book;
 
+import com.literatura.literal.chapter.ChapterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    public List<BookDTO> getAllBooks() {
+        return repository.findAll().stream().map(this::convertBookModelToDTO).toList();
+    }
+
+    private BookDTO convertBookModelToDTO(BookModel book) {
+        return new BookDTO(book);
+    }
 }
