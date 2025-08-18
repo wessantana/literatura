684781de426fad1ab28aad1132b5b0c615b1e496
wessantana <weslley.vieirasantana@gmail.com>
2025-08-18 20:12:26 -0300

    fix(chapter) replace lombok annotations for manual code to constructors, getters and setters

--- src/main/java/com/literatura/literal/chapter/ChapterDTO.java
+++ src/main/java/com/literatura/literal/chapter/ChapterDTO.java
@@ -1,23 +1,26 @@
 package com.literatura.literal.chapter;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.literatura.literal.book.BookDTO;
+import com.literatura.literal.book.BookModel;
 
-import java.util.UUID;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChapterDTO {
 
     private String title;
     private String content;
     private String bookTitle;
 
-    private ChapterDTO(ChapterModel chapter){
+    protected ChapterDTO() {}
+
+    public ChapterDTO(ChapterModel chapter, BookDTO book){
         this.title = chapter.getTitle();
         this.content = chapter.getContent();
-        this.bookTitle = chapter.getTitle();
+        this.bookTitle = book.getTitle();
     }
+
+    public String getTitle() {return title;}
+
+    public String getContent() {return content;}
+
+    public String getBookTitle() {return bookTitle;}
+
 }
