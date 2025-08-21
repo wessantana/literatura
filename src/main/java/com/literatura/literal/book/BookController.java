0282be876bb107e06fd6893b534dea8e99945114
wessantana <weslley.vieirasantana@gmail.com>
2025-08-20 22:13:30 -0300

    refactor(book): rewrite the main bookController endpoint

--- src/main/java/com/literatura/literal/book/BookController.java
+++ src/main/java/com/literatura/literal/book/BookController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/books")
 public class BookController {
 
     @Autowired
